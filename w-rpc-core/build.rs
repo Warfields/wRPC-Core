09ed9af71e0f87b7571d1786e6089d178490c719
Samuel Warfield <warfield@mymail.mines.edu>
2019-07-15 11:14:13 -0600

    Added automatic proto conversion

--- w-rpc-core/build.rs
+++ w-rpc-core/build.rs
@@ -1,14 +1,49 @@
 extern crate protoc_rust;
-
 use protoc_rust::Customize;
+use glob::glob;
 
 fn main() {
-	protoc_rust::run(protoc_rust::Args {
-	    out_dir: "src/protos",
-	    input: &["protos/init.proto"],
-	    includes: &["protos"],
-	    customize: Customize {
-	      ..Default::default()
-	    },
-	}).expect("protoc");
+    // Compile Main RPC Protos
+    let protos = glob("protos/*.proto").expect("No Proto Files Found");
+
+    for entry in protos {
+        match entry{
+            Ok(file) => {
+                protoc_rust::run(protoc_rust::Args {
+                    out_dir: "src/protos",
+                    input: &[file.into_os_string().into_string().unwrap().as_str()],
+                    includes: &["protos"],
+                    customize: Customize {
+                    ..Default::default()
+                    },
+                }).expect("protoc");
+                }
+
+            // if the path matched but was unreadable,
+            // thereby preventing its contents from matching
+            Err(e) => println!("{:?}", e),
+        }
+    }
+
+    // Compile Packager Specific Protos
+    let protos = glob("protos/packgers/*.proto").expect("No Proto Files Found");
+
+    for entry in protos {
+        match entry{
+            Ok(file) => {
+                protoc_rust::run(protoc_rust::Args {
+                    out_dir: "src/protos",
+                    input: &[file.into_os_string().into_string().unwrap().as_str()],
+                    includes: &["protos"],
+                    customize: Customize {
+                    ..Default::default()
+                    },
+                }).expect("protoc");
+                }
+                
+            // if the path matched but was unreadable,
+            // thereby preventing its contents from matching
+            Err(e) => println!("{:?}", e),
+        }
+    }
 }
