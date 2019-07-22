d6dd4bdddf09f880adad33191bf8c88b5b61e115
Samuel Warfield <warfield@mymail.mines.edu>
2019-07-22 12:48:13 -0600

    Moved RPC_Module::Module::fn_call to protos/mod.rs as it makes sense to be there instead of lib.rs

--- w-rpc-core/src/lib.rs
+++ w-rpc-core/src/lib.rs
@@ -20,16 +20,6 @@ use std::sync::Mutex;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-// Since the RPC_Module.rs code is generated from proto files the rpc call fn
-// is implemented here so it does not get overwritten;
-impl RPC_Module::Module {
-    // Macro?
-    pub fn fn_call(&self, fn_name: String, params: Vec<JsValue>) -> Result<JsValue, JsValue>{
-
-        Err(JsValue::from_str("Not Implimented yet!"))
-    }
-}
-
 // Create Global Module list
 lazy_static!{
     static ref GLOBAL_MODULE_LIST: Mutex<Vec::<RPC_Module::Module>>
--- w-rpc-core/src/protos/mod.rs
+++ w-rpc-core/src/protos/mod.rs
@@ -1,3 +1,15 @@
 pub mod RPC_Module;
 pub mod packager;
 pub mod result;
+use wasm_bindgen::prelude::*;
+
+// Since the RPC_Module.rs code is generated from proto files the rpc call fn
+// is implemented here so it does not get overwritten;
+impl RPC_Module::Module {
+    // Macro?
+    pub fn fn_call(&self, fn_name: String, params: Vec<JsValue>) -> Result<JsValue, JsValue>{
+
+        Err(JsValue::from_str("Not Implimented yet!"))
+    }
+}
+
